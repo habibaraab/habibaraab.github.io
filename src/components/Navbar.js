d2779a3f039d7972027628b19f7fad9a534a06a5
Habiba Rajab <135079683+habibaraab@users.noreply.github.com>
2024-07-06 00:36:50 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Abo-Eldaha Pizza</Link>
+    <Link className="navbar-brand" to="/"> Pizza</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
